e0a379d824fca45749ef8311747f9253d9759c25
daniprieto <dani101val@gmail.com>
2025-02-19 10:05:50 -0500

    Got the project running with basic configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Home from './components/home';
-import Login from './components/login';
-import Register from './components/register';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { createContext, useState } from "react";
+
+export const DataContext = createContext("");
 
 function App() {
+  const [logStatus, setLogStatus] = useState(0);
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </Router>
+    <DataContext.Provider value={{ logStatus, setLogStatus }}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/*" element={<Home />} />
+
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
+    </DataContext.Provider>
   );
 }
 
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,7 @@
 import { useState } from "react";
 export default function Home(){
     const [name, setName] = useState('');
-    //function nameHandler(e){
-    //    setName(e.target.value);
 
-    //}
     return(
         <div>
         <h1> Welcome to the Home Page {name}
